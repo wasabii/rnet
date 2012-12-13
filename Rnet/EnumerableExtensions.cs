e5f03d9643848b4476dc46f7755151ce30277c9f
Jerome Haltom <wasabi@larvalstage.net>
2012-12-13 00:12:24 -0600

    * Throw typed exceptions. * Implement event raising on new data.

---
+++ Rnet/EnumerableExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Rnet
+{
+
+    static class EnumerableExtensions
+    {
+
+        /// <summary>
+        /// Gets the next byte.
+        /// </summary>
+        /// <param name="e"></param>
+        /// <returns></returns>
+        internal static byte Next(this IEnumerator<byte> e)
+        {
+            if (!e.MoveNext())
+                throw new EndOfStreamException();
+
+            return e.Current;
+        }
+
+    }
+
+}
