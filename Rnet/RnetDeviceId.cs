fb573d8f61bff2d160f4508cfeb58fad2973cbf0
Jerome Haltom <wasabi@larvalstage.net>
2013-06-10 11:19:47 -0500

    Use structs.

--- Rnet/RnetDeviceId.cs
+++ Rnet/RnetDeviceId.cs
@@ -11,7 +11,7 @@ namespace Rnet
         /// <summary>
         /// <see cref="RnetDeviceId"/> which targets the controller.
         /// </summary>
-        public static readonly RnetDeviceId RootControllerTarget = new RnetDeviceId(0, 0, (byte)RnetKeypadId.Controller);
+        public static readonly RnetDeviceId RootControllerTarget = new RnetDeviceId(0, 0, RnetKeypadId.Controller);
 
         /// <summary>
         /// <see cref="RnetDeviceId"/> recommended by Russound for external control systems.
@@ -24,7 +24,7 @@ namespace Rnet
         /// <param name="controllerId"></param>
         /// <param name="zoneId"></param>
         /// <param name="keypadId"></param>
-        public RnetDeviceId(byte controllerId, byte zoneId, byte keypadId)
+        public RnetDeviceId(RnetControllerId controllerId, RnetZoneId zoneId, RnetKeypadId keypadId)
             : this()
         {
             ControllerId = controllerId;
@@ -35,17 +35,17 @@ namespace Rnet
         /// <summary>
         /// ID of the controller.
         /// </summary>
-        public byte ControllerId { get; private set; }
+        public RnetControllerId ControllerId { get; private set; }
 
         /// <summary>
         /// ID of the zone.
         /// </summary>
-        public byte ZoneId { get; private set; }
+        public RnetZoneId ZoneId { get; private set; }
 
         /// <summary>
         /// ID of the keypad.
         /// </summary>
-        public byte KeypadId { get; private set; }
+        public RnetKeypadId KeypadId { get; private set; }
 
         /// <summary>
         /// Writes the device ID to the writer.
